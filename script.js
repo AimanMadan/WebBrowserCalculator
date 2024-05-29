381b91a292843c11a9180fa85c7563f74ced457c
Aiman <64807286+AimanMadan@users.noreply.github.com>
2024-05-28 23:29:07 -0700

    Updates

--- script.js
+++ script.js
@@ -1,46 +1,90 @@
 class Calculator {
-    constructor(firstOpandTextElement, secondOpandTextElement) {
-        this.firstOpandTextElement = firstOpandTextElement
-        this.secondOpandTextElement = secondOpandTextElement
+    constructor(firstOpTextElement, secondOpTextElement) {
+        this.firstOpTextElement = firstOpTextElement  
+        this.secondOpTextElement = secondOpTextElement  
         this.clear()
     }
 
     clear() {
-
-        this.first_op ='';
-        this.second_op ='';
+        this.first_op = '';  // Initialized the operands
+        this.second_op = '';  // Initialized the operands
         this.op = undefined;
     }
 
     delete() {
-        
+        this.second_op = this.second_op.toString().slice(0, -1)
     }
 
     appendNumber(number) {
-
+        
+        if(number === '.' && this.second_op.includes('.')) return //limits one time use for the "."
+        this.second_op = this.second_op.toString() + number.toString();  // Appended the number to the second operand
     }
 
-    chooseOp(op) {
+    chooseoperation(operation) {
+        if (this.second_op === '') return;
+        if (this.first_op !== '') {
+            this.compute()
+        }
 
-    }
+        this.operation = operation
+        this.first_op = this.second_op
+        this.second_op = ''
+        }
+
+    
 
     compute() {
-
+        // Implementation here
     }
 
     updateDisplay() {
-
+        this.secondOpTextElement.innerText = this.second_op;
+        this.firstOpTextElement.innerText = this.first_op;
     }
 }
 
+//linking constants to the HTML Elements
 
-
+//Buttons HTML --> JS
 const numberButtons = document.querySelectorAll('[data-number]')
-const opButtons = document.querySelectorAll('[data-op]')
-const equalsButtons = document.querySelector('[data-equals]')
-const delButtons = document.querySelector('[data-del]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const delButton = document.querySelector('[data-del]')
 const ACButton = document.querySelector('[data-all-clear]')
 
-const firstOpandTextElement = document.querySelector('[data-first_op]')
-const secondOpandTextElement = document.querySelector('[data-second_op]')
+//Operands HTML --> JS
+const firstOpTextElement = document.querySelector('[data-first_op]')
+const secondOpTextElement = document.querySelector('[data-second_op]')
 
+//Creating a new calculator object and passing the first and second operand elements to it
+const calculator = new Calculator(firstOpTextElement, secondOpTextElement) 
+
+
+//Numbers Buttons
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})  
+
+//Operations Buttons
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseoperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+//All-Clear Button
+    ACButton.addEventListener('click', button => {
+        calculator.clear()
+        calculator.updateDisplay()
+    })
+
+//delete Button
+    delButton.addEventListener('click', button => {
+        calculator.delete()
+        calculator.updateDisplay()
+    })
